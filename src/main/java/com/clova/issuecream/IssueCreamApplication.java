0a18ba7624d2b0181af368e2f48cc495849a485b
Jinuk00 <okman3000@naver.com>
2024-05-22 10:37:25 +0900

    project 기본 구성

---
+++ src/main/java/com/clova/issuecream/IssueCreamApplication.java
@@ -0,0 +1,13 @@
+package com.clova.issuecream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueCreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IssueCreamApplication.class, args);
+    }
+
+}
