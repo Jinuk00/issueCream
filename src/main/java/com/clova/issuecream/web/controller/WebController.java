6f7adc306d672a53b765162dde60eb1cccfce305
Jinuk00 <okman3000@naver.com>
2024-05-24 09:52:21 +0900

    배포 설정

---
+++ src/main/java/com/clova/issuecream/web/controller/WebController.java
@@ -0,0 +1,12 @@
+package com.clova.issuecream.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+//    @GetMapping("/")
+//    public String reactRoot() {
+//        return "/index.html";
+//    }
+}
