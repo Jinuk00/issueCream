48740e78ba701c64a962b9fdbbe0feb504128163
Jinuk00 <okman3000@naver.com>
2024-05-24 11:37:03 +0900

    배포 오류수정2

--- src/main/java/com/clova/issuecream/proxyTest/TestController.java
+++ src/main/java/com/clova/issuecream/proxyTest/TestController.java
@@ -1,12 +1,11 @@
 package com.clova.issuecream.proxyTest;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class TestController {
 
     @PostMapping("/test/proxy")
