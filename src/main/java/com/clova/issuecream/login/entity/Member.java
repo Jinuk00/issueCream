d9d899b3b097c33bda64a9949f63c7414bbfc30b
Jinuk00 <okman3000@naver.com>
2024-05-31 14:58:10 +0900

    db 설정 및 JPA 설정 추가

--- src/main/java/com/clova/issuecream/login/entity/Member.java
+++ src/main/java/com/clova/issuecream/login/entity/Member.java
@@ -1,24 +1,46 @@
 package com.clova.issuecream.login.entity;
 
 import com.clova.issuecream.login.enums.Role;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+import org.hibernate.annotations.DynamicUpdate;
 
+@Entity
+@Table(name = "MEMEBER")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DynamicUpdate
 public class Member {
-    long id;
+
+    @Id
+    @Column(name = "seq")
+    @Comment("순번")
+    long seq;
+
+    @Column(name = "name")
+    @Comment("이름")
     String name;
+    @Column(name = "username")
+    @Comment("아이디")
     String username; //id
+    @Column(name = "email")
+    @Comment("이메일")
     String email;
+    @Column(name = "role")
+    @Comment("권한")
     Role role;
 
 
     @Builder
-    public Member(long id, String name, String username, String email, Role role) {
-        this.id = id;
+    public Member(long seq, String name, String username, String email, Role role) {
+        this.seq = seq;
         this.name = name;
         this.username = username;
         this.email = email;
