0f3ddbd2348e860e0839206c5d50402f0a9fad1c
Jinuk00 <okman3000@naver.com>
2024-06-03 15:53:38 +0900

    배포 시 오류로 인한 security 수정

--- src/main/java/com/clova/issuecream/config/SecurityConfig.java
+++ src/main/java/com/clova/issuecream/config/SecurityConfig.java
@@ -42,6 +42,8 @@ public class SecurityConfig {
                 .requestMatchers(AntPathRequestMatcher.antMatcher("/error"), AntPathRequestMatcher.antMatcher("/favicon.ico"),
                         AntPathRequestMatcher.antMatcher("/index.html"),
                         AntPathRequestMatcher.antMatcher("/js/**"),
+                        AntPathRequestMatcher.antMatcher("/css/**"),
+                        AntPathRequestMatcher.antMatcher("/images/**"),
                         PathRequest.toStaticResources().atCommonLocations());
     }
 
