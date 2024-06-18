38801fe928e584e93ebc02c50c5f6777a295baf6
ShinMS1231 <114546660+ShinMS1231@users.noreply.github.com>
2024-06-18 23:01:16 +0900

    이미지 크기 수정
    이미지 크기 수정

--- src/main/frontend/issue-cream-web/src/page/UserInfo.js
+++ src/main/frontend/issue-cream-web/src/page/UserInfo.js
@@ -60,7 +60,7 @@ function UserInfo() {
                     로그인 정보
                 </div>
                 <div style={{marginBottom: '2rem'}}>
-                    <img src="/images/profile.png" className="user-ixnfo-size"/>
+                    <img src="/images/profile.png" className="user-info-size"/>
                 </div>
                 <div style={{marginBottom: '2rem'}}>
                     카카오톡 아이디: {email}
